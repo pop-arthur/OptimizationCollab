4d7aaf911a34d69cd53e1b9294288393f453b1fa
GEEG <e.glebov@innopolis.university>
2024-09-17 10:41:20 +0300

    Second version of input

--- simplex_method/main.py
+++ simplex_method/main.py
@@ -1,51 +1,86 @@
-class Vector:
-    row = []
-    numb_of_rows = 0
-    def __init__(self, row):
-        self.row = row
-        self.numb_of_columns = len(row)
+from typing import Tuple
+
+
+class Vector: 
+    """
+    Class of vectors
+    """
+    def __init__(self, row=None):
+        """
+        You may initialise vector with known numbers - row, or without them
+        """
+        if row is None:
+            self.row = []
+        else:
+            self.row = row
+        self.numb_of_columns = len(self.row)
+
     def input(self):
-        temp = input()
-        self.row = temp.split(' ')
-        self.numb_of_rows = len(self.row)
-    def __init__(self):
-        self.row = []
-        self.numb_of_rows = 0
-    def input_rows(self, row):
-        self.row = row.split(' ')
-        self.numb_of_rows = len(row)
+        """
+        Function to get user input and handle it. Parse input by itself
+        """
+        temp = input("Enter the vector (space-separated values): ")
+        self.row = list(map(float, temp.split()))  # Convert input to list of floats
+        self.numb_of_columns = len(self.row)
+        return self
+
 class Matrix:
-    matrix = [Vector]
-    numb_of_columns = 0
-    numb_of_rows = 0
-    def __init__(self, matrix):
+    """
+    Class of matrix
+    """
+    def __init__(self, matrix=None):
+        """
+        You may initialise vector with known numbers - matrix, or without them
+        """
+        if matrix is None:
+            self.matrix = []
+        else:
+            self.matrix = matrix
+        self.numb_of_rows = len(self.matrix)
+        self.numb_of_columns = len(self.matrix[0]) if self.numb_of_rows > 0 else 0
+
+    def input(self):
+        """
+        Function to get user input and handle it. Parse input by itself
+        """
+        print("Enter the matrix row by row (press Enter twice to stop):")
+        matrix = []
+        while True:
+            temp = input()
+            if temp == "":  # Stop input when user presses enter twice
+                break
+            row = list(map(float, temp.split()))  # Convert input row to list of floats
+            matrix.append(row)
         self.matrix = matrix
         self.numb_of_rows = len(matrix)
-        self.numb_of_columns = len(matrix[0])
-    def __init__(self):
-        self.matrix = [[]]
-        self.numb_of_rows = 0
-        self.numb_of_columns = 0
-    def input(self):
-        list = [[]]
-        temp = input()
-        while temp != "":
-            list.append(Vector.input_rows(Vector(), temp))
-            temp = input()
-        self.matrix = list
-        self.numb_of_rows = len(list)
-        self.numb_of_columns = len(list[0])
-        
-def input_variables():
-    print('First of all write a vector of coefficients of objective function!')
+        self.numb_of_columns = len(matrix[0]) if self.numb_of_rows > 0 else 0
+        return self
+
+def input_variables() -> Tuple[Vector, Matrix, Vector, float]:
+    """
+    Function to get all user input. Return all of data in classes and one - in float
+    """
+    print('First, enter a vector of coefficients for the objective function:')
     vcof = Vector().input()
-    print('Now write a matrix of coefficients of constraint function')
+
+    print('Now, enter a matrix of coefficients for the constraint function:')
     mccf = Matrix().input()
-    print('After that write a vector of right-hand side numbers')
+
+    print('Next, enter a vector of right-hand side numbers:')
     vrhsn = Vector().input()
-    print('Last one, write the approximation acuracy')
-    apac = int(input()) 
-    print(vcof.row, mccf, vrhsn, apac)
+
+    print('Lastly, enter the approximation accuracy:')
+    apac = float(input())
+    #for dev purpouse only. Delete before prod.
+    print("Objective Function Coefficients:", vcof.row)
+    print("Constraint Matrix:")
+    for row in mccf.matrix:
+        print(row)
+    print("Right-Hand Side Vector:", vrhsn.row)
+    print("Approximation Accuracy:", apac)
+    #end of block to be deleted
+    return vcof, mccf, vrhsn, apac
+
 if __name__ == '__main__':
     input_variables()
-    print("Welcome to Hell")
+    print("Welcome to Hell!")
