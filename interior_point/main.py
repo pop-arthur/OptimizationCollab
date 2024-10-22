d118d154659b64df72ccf6429c557c1a83c7b82b
GEEG <e.glebov@innopolis.university>
2024-10-22 13:36:10 +0300

    Minor fix of import

--- interior_point/main.py
+++ interior_point/main.py
@@ -1,4 +1,4 @@
-from objects import Matrix
-A = Matrix.Matrix(3,3,[[1, 2, 3], [0, 1, 4], [0, 0, 1]])
+from objects import *
+A = Matrix(3,3,[[1, 2, 3], [0, 1, 4], [0, 0, 1]])
 #TODO Solve problems with import
 print(A.inverse())
