711edf7fc058afd5f6084d5a2768718814e8d75a
GEEG <e.glebov@innopolis.university>
2024-10-22 12:37:19 +0300

    Great old matrix from AGLA II is back

--- interior_point/objects/matrix.py
+++ interior_point/objects/matrix.py
@@ -1,78 +1,11 @@
-from typing import Optional
-
-
 class Matrix:
-    """
-    Class of matrix
-    """
-
-    def __init__(self, matrix: Optional['Matrix'] = None):
-        """
-        initialisation of matrix
-
-        :param matrix: Known matrix to be equal with new matrix
-        """
-        if matrix is None:
-            self.matrix = []
+    def __init__(self, rows, cols, numbers=None):
+        self.rows = rows
+        self.columns = cols
+        if numbers:
+            self.numbers = numbers
         else:
-            self.matrix = matrix
-        self.numb_of_rows = len(self.matrix)
-        self.numb_of_columns = len(self.matrix[0]) if self.numb_of_rows > 0 else 0
-
-
-    def input(self) -> 'Matrix':
-        """
-        Function to get user input and handle it. Parse input by itself
-        :return: Matrix with user input
-        """
-        print("Enter the matrix row by row (press Enter twice to stop):")
-        matrix = []
-        while True:
-            temp = input()
-            if temp == "":  # Stop input when user presses enter twice
-                break
-            row = list(map(float, temp.split()))  # Convert input row to list of floats
-            matrix.append(row)
-        self.matrix = matrix
-        self.numb_of_rows = len(matrix)
-        self.numb_of_columns = len(matrix[0]) if self.numb_of_rows > 0 else 0
-        return self
-    
-
-    def transpose(self) -> 'Matrix':
-        """
-        Function to transpose matrix
-
-        :return: Transpose of matrix
-        """
-        result = Matrix(self.columns, self.rows)
-        for i in range(self.rows):
-            for j in range(self.columns):
-                result.numbers[j][i] = self.numbers[i][j]
-        return result
-    
-
-    def inverse(self):
-        #TODO write
-        pass
-
-
-    def __add__(self, other: 'Matrix') ->  'Matrix':
-        """
-        Adds two matrices and returns a new Matrix object.
-        
-        :param other: Another matrix to add to this one.
-        :return: A new matrix containing the sum of both matrices.
-        """
-        if self.rows == other.rows and self.columns == other.columns:
-            result = Matrix(self.rows, self.columns)
-            for i in range(self.rows):
-                for j in range(self.columns):
-                    result.numbers[i][j] = self.numbers[i][j] + other.numbers[i][j]
-            return result
-        else:
-            raise ValueError("Error: the dimensional problem occurred")
-    
+            self.numbers = [[0 for _ in range(cols)] for _ in range(rows)]
 
     def __sub__(self, other):
         if self.rows == other.rows and self.columns == other.columns:
@@ -82,9 +15,18 @@ class Matrix:
                     result.numbers[i][j] = self.numbers[i][j] - other.numbers[i][j]
             return result
         else:
-            raise ValueError("Error: the dimensional problem occurred")
+            raise Exception("Error: the dimensional problem occurred")
 
-    
+    def __add__(self, other):
+        if self.rows == other.rows and self.columns == other.columns:
+            result = Matrix(self.rows, self.columns)
+            for i in range(self.rows):
+                for j in range(self.columns):
+                    result.numbers[i][j] = self.numbers[i][j] + other.numbers[i][j]
+            return result
+        else:
+            raise Exception("Error: the dimensional problem occurred")
+
     def __mul__(self, other):
         if self.columns == other.rows:
             result = Matrix(self.rows, other.columns)
@@ -93,30 +35,16 @@ class Matrix:
                     result.numbers[i][j] = sum(self.numbers[i][k] * other.numbers[k][j] for k in range(self.columns))
             return result
         else:
-            raise ValueError("Error: the dimensional problem occurred")
-        
-    
-    def diag(self) -> 'Matrix': 
-        """
-        Function to get diagonal of matrix.
+            raise Exception("Error: the dimensional problem occurred")
 
-        :return: A new matrix with elements only on the diagonal
-        """
-        res = []
-        for i in range(self.numb_of_columns):
-            row  = []
-            for  j  in range(self.numb_of_rows):
-                if (i==j):
-                    row.append(self.matrix[i][j])
-                else:
-                    row.append(0)
-            res.append(row)
-        return Matrix(res)
-    
+    def transpose(self):
+        result = Matrix(self.columns, self.rows)
+        for i in range(self.rows):
+            for j in range(self.columns):
+                result.numbers[j][i] = self.numbers[i][j]
+        return result
 
-    def det(self):
-        if (self.numb_of_columns != self.numb_of_rows):
-            raise ValueError("Matrix must be squared to find determinant")
+    def determinant(self):
         temp = [row[:] for row in self.numbers]
         n = len(temp)
         det = 1
@@ -141,30 +69,76 @@ class Matrix:
             det *= temp[i][i]
         return det
 
-    def __repr__(self) -> str:
-        """
-        Function to make string output from data in instance of class
+    def identity(self):
+        if self.rows != self.columns:
+            raise Exception("Error: Identity matrix must be square.")
+        identity_matrix = Matrix(self.rows, self.rows)
+        for i in range(self.rows):
+            identity_matrix.numbers[i][i] = 1
+        return identity_matrix
 
-        :return: string with data and formating
-        """
-        return f"{self.matrix}"
+    def nullify(self, row, col, row_to_start):
+        row -= 1
+        col -= 1
+        for j in range(row_to_start, self.rows):
+            if j != row:
+                if self.numbers[row][col] != 0 and self.numbers[j][col] != 0:
+                    multiplier = self.numbers[row][col] / self.numbers[j][col]
+                    for i in range(self.columns):
+                        self.numbers[row][i] -= self.numbers[j][i] * multiplier
 
+    def exchange(self, i1, i2):
+        i1 -= 1
+        i2 -= 1
+        if i1 < self.rows and i2 < self.rows:
+            self.numbers[i1], self.numbers[i2] = self.numbers[i2], self.numbers[i1]
+        else:
+            raise Exception("Error: Index out of bounds.")
 
-    @staticmethod
-    def identity(size:int) -> 'Matrix':
-        """
-        Function to return identity matrix of needed size.
+    def inverse(self):
+        if self.rows != self.columns:
+            raise Exception("Error: Matrix must be square for inversion.")
 
-        :param size: Size of identity matrix
-        :return: A new identity matrix
-        """
-        res = []
-        for i in range(size):
-            row  = []
-            for  j  in range(size):
-                if (i==j):
-                    row.append(1)
-                else:
-                    row.append(0)
-            res.append(row)
-        return Matrix(res)
+        temp = Matrix(self.rows, self.columns, [row[:] for row in self.numbers])
+        identity_matrix = self.identity()
+        n = temp.rows
+
+        for i in range(n):
+            pivot = i
+            for j in range(i + 1, n):
+                if abs(temp.numbers[j][i]) > abs(temp.numbers[pivot][i]):
+                    pivot = j
+
+            if pivot != i:
+                temp.exchange(i + 1, pivot + 1)
+                identity_matrix.exchange(i + 1, pivot + 1)
+
+            for j in range(i + 1, n):
+                if temp.numbers[j][i] != 0:
+                    factor = temp.numbers[j][i] / temp.numbers[i][i]
+                    for k in range(n):
+                        temp.numbers[j][k] -= factor * temp.numbers[i][k]
+                        identity_matrix.numbers[j][k] -= factor * identity_matrix.numbers[i][k]
+
+        for i in range(n - 1, -1, -1):
+            for j in range(i - 1, -1, -1):
+                if temp.numbers[j][i] != 0:
+                    factor = temp.numbers[j][i] / temp.numbers[i][i]
+                    for k in range(n):
+                        temp.numbers[j][k] -= factor * temp.numbers[i][k]
+                        identity_matrix.numbers[j][k] -= factor * identity_matrix.numbers[i][k]
+
+        for i in range(n):
+            div = temp.numbers[i][i]
+            for j in range(n):
+                temp.numbers[i][j] /= div
+                identity_matrix.numbers[i][j] /= div
+
+        return identity_matrix
+
+    def __str__(self):
+        matrix_str = ''
+        for row in self.numbers:
+            matrix_str += ' '.join(f"{x:.2f}" for x in row) + '\n'
+        return matrix_str.strip()
+
