30a7810f8c908f408aaaf0ddfcc783a79c4f6458
GEEG <e.glebov@innopolis.university>
2024-10-27 16:56:21 +0300

    Add vectors to input, no need to press enter twice

--- interior_point/input_vars.py
+++ interior_point/input_vars.py
@@ -3,16 +3,16 @@ from typing import Tuple
 
 def input_vars() -> Tuple[Matrix, Matrix, Matrix, Matrix, float]:
     print('Enter the vector of coefficients for the objective function (C):')
-    vcof = Matrix().input()
+    vcof = Matrix().input(vector_input=True)
 
     print('Enter the matrix of coefficients for the constraint function (A):')
     mccf = Matrix().input()
 
     print('Enter your initial starting point (as a vector):')
-    inix = Matrix().input()
+    inix = Matrix().input(vector_input=True)
 
     print('Enter the vector of right-hand side numbers (b):')
-    vrhsn = Matrix().input()
+    vrhsn = Matrix().input(vector_input=True)
 
     print('Enter the approximation accuracy (ϵ):')
     apac = float(input("Accuracy ε: "))
